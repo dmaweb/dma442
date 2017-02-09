5cf8c347bda778d1cc0b973bcb048c81d518f7cd
dmaweb <dunkleb@canisius.edu>
2017-02-09 16:45:50 -0500

    worked on ex1

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,6 @@
 /* dependencies */
 var gulp = require('gulp');
 var sass = require('gulp-sass');
-var sourcemaps = require('gulp-sourcemaps');
-var autoprefixer = require('gulp-autoprefixer');
 var browserSync = require('browser-sync').create();
 
 /* options */
@@ -13,7 +11,6 @@ var autoprefixerOptions = { browsers: ['last 2 versions', '> 5%'] };
 
 gulp.task('browser-sync', ['sass'], function() {
 	browserSync.init({
-		 reloadDelay: 100,
 		server: {
 			baseDir: './'
 		}
@@ -26,18 +23,10 @@ gulp.task('browser-sync', ['sass'], function() {
 gulp.task('sass', function() {
 	return gulp
 		.src('./**/*.scss')
-		.pipe(sourcemaps.init())
 		.pipe(sass(sassOptions).on('error', sass.logError))
-		.pipe(autoprefixer(autoprefixerOptions))
-		.pipe(sourcemaps.write('./'))
 		.pipe(gulp.dest('./'))
 		.pipe(browserSync.stream());
 });
-
-
-
-
-
 
 /* default task, if you just type gulp in the terminal */
 gulp.task('default', ['browser-sync']);
