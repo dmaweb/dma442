970a7b958972afc9c42420a78d455d980f5db9d4
Ben Dunkle <ben@field2.com>
2017-03-28 15:35:53 -0400

    Added cw11a

---
+++ cw11a/header.php
@@ -0,0 +1,8 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8" />
+	<title>Learning PHP : <?php echo $page_title; ?></title>
+</head>
+<body>
+<h1><?php echo $page_title; ?></h1>
---
+++ cw11a/includes.php
@@ -0,0 +1,4 @@
+<?php $page_title = "PHP is great for templates." ?>
+<?php include('header.php'); ?>
+<p>PHP can include other files and pass them variables.</p>
+<?php include('footer.php'); ?>
