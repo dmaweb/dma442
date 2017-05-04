9a85fc94096c62199d525ea4a03543a852c05736
Ben Dunkle <ben@field2.com>
2017-05-04 15:24:35 -0400

    added testimonials post type

--- ben/functions.php
+++ ben/functions.php
@@ -14,3 +14,22 @@ register_sidebar(array(
     'name' 	=> 'Footer widgets',
 )); // enable widgets in our theme that can be added to the footer area
 
+
+// create testimonials custom post type
+	register_post_type('testimonials',
+		array(
+			'labels' => array(
+				'name' => __('Testimonials'),
+				'singular_name' => __('testimonial'),
+			),
+			'public' => true,
+			'has_archive' => true,
+			'menu_icon' => 'dashicons-format-quote',
+			'capability_type' => 'post',
+			'rewrite' => true,
+		)
+	);
+
+
+
+add_action('init', 'codex_custom_init');
