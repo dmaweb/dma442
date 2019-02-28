7cb0f1ef049769eabbb1771fab06507bd87c39ee
Ben Dunkle <ben@field2.com>
2019-02-28 14:45:35 -0500

    lab4 updated

--- lab4/js/script.js
+++ lab4/js/script.js
@@ -1,7 +1,22 @@
 
 $(document).ready(function() {
 
-})
+$( "nav" ).click(function() {
+  event.stopPropagation();
+});
+
+$( "#hamburger" ).click(function() {
+  $( ".menu" ).toggleClass( "visible" );
+});
+
+$("body").click(function() {
+	  $( ".menu" ).removeClass( "visible" );
+});
+
+});
 
 
+
+
+  
 
