d27fde3baf368e3a1f269b17c3d4efa93b338801
Ben Dunkle <ben@field2.com>
2019-04-02 14:13:17 -0400

    Added lab 6

---
+++ lab6/about.php
@@ -0,0 +1,16 @@
+<?php 
+include("header.php");
+?>
+
+	Hello World
+	Type your password to
+	access your bank account:
+	<input type="password">
+	<input type="submit">
+
+	<?php 
+echo "howdy";
+$password = "secretPass";
+	?>
+</body>
+</html>
---
+++ lab6/index.php
@@ -0,0 +1,11 @@
+<?php 
+include("header.php");
+?>
+
+	Hello World
+	<?php 
+echo "howdy";
+$password = "secretPass";
+	?>
+</body>
+</html>
